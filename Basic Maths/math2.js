7c55c47f468f930502656cc61a4d42ad489553bb
anju <codebyanju@gmail.com>
2024-08-23 10:36:56 +0530

    countOddDigit

---
+++ Basic Maths/math2.js
@@ -0,0 +1,19 @@
+class Solution {
+    countOddDigit(n) {
+
+        let count = 0;
+
+        while(n>0){
+
+            let lastDigit = n % 10;
+            if(lastDigit % 2 !== 0) count++
+            n = Math.floor(n / 10)
+        }
+        return count
+
+    }
+}
+
+const sol = new Solution();
+let ans = sol.countOddDigit(12345);
+console.log(ans);

a4dfd37e7f2c3cce2452639023ea4b7708804767
anju <codebyanju@gmail.com>
2024-08-23 22:03:45 +0530

    reverseNumber

---
+++ Basic Maths/math3.js
@@ -0,0 +1,19 @@
+class Solution {
+    reverseNumber(n) {
+        let revNum = 0
+
+        while(n > 0){
+            let lastDigit = n % 10;
+
+            revNum = (revNum * 10) + lastDigit
+            n = Math.floor(n/10)
+
+        }
+        return revNum
+
+    }
+}
+
+const sol = new Solution()
+const ans = sol.reverseNumber(1234)  
+console.log(ans)

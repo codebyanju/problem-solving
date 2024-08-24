65953f4c6289631726355cd027096bf7c5feb1ac
anju <codebyanju@gmail.com>
2024-08-24 14:50:16 +0530

    isperfect

--- Basic Maths/8_isPerfect.js
+++ Basic Maths/8_isPerfect.js
@@ -1,7 +1,7 @@
 class Solution {
   isPerfect(n) {
     let sum = 0;
-    for (let i = 0; i < n; i++) {
+    for (let i = 1; i < n; i++) {
       if (n % i == 0) {
         sum = sum + i;
       }
@@ -17,10 +17,12 @@ class Solution2 {
     let sum = 1;
 
     for (let i = 2; i * i <= n; i++) {
+      // starts from 2 and hence we have avoided 1 * 36 case
       if (n % i == 0) {
         sum = sum + i;
 
         if (i !== n / i) {
+          // so we dont need to check for n !== n/i here since our loop starts from 2
           // 6 !== 36/6 ---> avoid 6 * 6
           sum = sum + n / i;
         }
@@ -30,13 +32,40 @@ class Solution2 {
   }
 }
 
+class Solution3 {
+  isPerfect(n) {
+    if (n <= 1) return false;
+
+    let sum = 0;
+    let copy = n;
+
+    for (let i = 1; i <= Math.sqrt(n); i++) {
+      if (n % i == 0) {
+        sum = sum + i;
+
+        // (n/i == n) ---> avoiding the num itself as divisor 1 * 36 = 36
+        // (i !== n/i ---> avoiding same divisor 6 * 6 = 36
+
+        if (i !== n / i && n !== n / i) {
+          sum = sum + Math.floor(n / i);
+        }
+      }
+    }
+    return sum == copy;
+  }
+}
+
 const sol = new Solution();
-const ans = sol.isPerfect(6);
+const ans = sol.isPerfect(28);
 console.log(ans);
 
 const sol2 = new Solution2();
 const ans2 = sol2.isPerfect(28);
 console.log(ans2);
+
+const sol3 = new Solution3();
+const ans3 = sol3.isPerfect(28);
+console.log(ans3);
 
 // TC = O(sqrt(N)) - Running a loop from 1 to square root of N.
 // SC - O(1) - Using a couple of variables i.e., constant space.
